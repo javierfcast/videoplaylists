facc225871a6dee3d74580d5b9b6baa7a8b1258d
Javier Castillo <javier@nadd.co>
2018-01-27 10:49:35 -0600

    UPDATED: browse.js // made featured playlists responsive

--- src/components/browse.js
+++ src/components/browse.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import styled from 'styled-components';
+import { css } from 'styled-components';
 
 //Import Images
 import imgCardi from '../images/featured/featured_cardi.jpg';
@@ -9,6 +10,23 @@ import imgArcadeFire from '../images/featured/featured_arcade_fire.jpg';
 // import imgDespacito from '../images/featured/featured_despacito.jpg';
 import imgOzuna from '../images/featured/featured_ozuna.jpg';
 import imgQuavo from '../images/featured/featured_quavo.jpg';
+
+const sizes = {
+  small: 360,
+  xmedium: 720,
+  xlarge: 1200
+}
+
+// Iterate through the sizes and create a media template
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+		@media (min-width: ${sizes[label] / 16}em) {
+			${css(...args)}
+		}
+	`
+  return acc
+}, {})
+
 
 const PlaylistsContainer = styled.div`
   padding: 20px;
@@ -73,7 +91,6 @@ const PlaylistActionsNone = styled.span`
 //Featured View Classes
 
 const StyledFeaturedContainer = styled.div`
-  max-width: 960px;
   margin: 0 auto;
   padding-top: 20px;
   padding-bottom: 20px;
@@ -84,6 +101,7 @@ const StyledFeaturedHero = styled.div`
 `;
 const StyledFeaturedGrid = styled.div`
   display: flex;
+  flex-wrap: wrap;
 `;
 const StyledFeatured = styled(Link)`
   position: relative;
@@ -93,8 +111,17 @@ const StyledFeatured = styled(Link)`
   border: 1px solid rgba(255,255,255,0.1);
   transition: all .3s ease;
   display: block;
+  width: 100%;
   &:hover{
     border: 1px solid rgba(255,255,255,255.1);
+  }
+  &.col-33{
+    ${media.xmedium`
+      width: 50%; 
+    `}
+    ${media.xlarge`
+      width: 33.333%;
+    `}
   }
   img{
     display: block;
@@ -105,11 +132,17 @@ const StyledFeatured = styled(Link)`
   }
 `;
 const StyledFeaturedContent = styled.div`
-  position: relative;
+  position: absolute;
   padding: 20px;
-  margin-top: -84px;
+  bottom: 0;
   h2{
-    
+    font-size: 14px;
+    ${media.xmedium`
+      font-size: 18px;
+    `}
+    ${media.xlarge`
+      font-size: 24px;
+    `}
   }
   span{
     letter-spacing: 2px;
@@ -232,7 +265,7 @@ class Browse extends Component {
             <StyledFeaturedContainer>
               <StyledFeaturedHero>
                 <StyledFeatured to="/users">
-                  <img src={imgCardi} />
+                  <img src={imgCardi} alt="Cardi" />
                   <StyledFeaturedContent>
                     <h2>Pitchfork's best 100 songs of 2017</h2>
                     <span>By Video Playlist</span>
@@ -240,22 +273,22 @@ class Browse extends Component {
                 </StyledFeatured>
               </StyledFeaturedHero>
               <StyledFeaturedGrid>
-                <StyledFeatured to="/users">
-                  <img src={imgQuavo} />
+                <StyledFeatured className="col-33" to="/users">
+                  <img src={imgQuavo} alt="Quavo" />
                   <StyledFeaturedContent>
                     <h2>Rap Caviar</h2>
                     <span>By Video Playlist</span>
                   </StyledFeaturedContent>
                 </StyledFeatured>
-                <StyledFeatured to="/users">
-                  <img src={imgOzuna} />
+                <StyledFeatured className="col-33" to="/users">
+                  <img src={imgOzuna} alt="Ozuna" />
                   <StyledFeaturedContent>
                     <h2>Fiesta Latina</h2>
                     <span>By Video Playlist</span>
                   </StyledFeaturedContent>
                 </StyledFeatured>
-                <StyledFeatured to="/users">
-                  <img src={imgArcadeFire} />
+                <StyledFeatured className="col-33" to="/users">
+                  <img src={imgArcadeFire} alt="Arcade Fire" />
                   <StyledFeaturedContent>
                     <h2>Indie Mornings</h2>
                     <span>By Video Playlist</span>
