d1c51a00d205c2c808c9b32157d5ad6337a09422
Kevin Chanquin <chanquinkevin@gmail.com>
2018-03-11 19:02:20 -0600

    REMOVED Unnecessary constant

--- src/components/sortable_component.js
+++ src/components/sortable_component.js
@@ -1,24 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { css } from 'styled-components';
 
 import {SortableContainer, SortableElement, arrayMove, SortableHandle} from 'react-sortable-hoc';
-
-const sizes = {
-  small: 360,
-  xmedium: 720,
-  xlarge: 1200
-}
-
-// Iterate through the sizes and create a media template
-const media = Object.keys(sizes).reduce((acc, label) => {
-  acc[label] = (...args) => css`
-		@media (min-width: ${sizes[label] / 16}em) {
-			${css(...args)}
-		}
-	`
-  return acc
-}, {})
 
 //custom components
 
