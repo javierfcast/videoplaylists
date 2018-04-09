f7df8a04d54dbe4b400cd32fb086367611b189b2
Kevin Chanquín <kevin@royalestudios.com>
2018-04-09 15:14:38 -0600

    Revert "UPDATE video_item // Convert component to class"
    This reverts commit 7672ed185e9c9c0905f816642b0f6bd0d8af4ef7.

--- src/components/video_item.js
+++ src/components/video_item.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import MaterialIcon from 'material-icons-react';
 import Moment from 'moment';
@@ -96,116 +96,107 @@ const StyledLibraryButtonCheck = styled.a`
   }
 `
 
-class VideoItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      optionsOpen: false,
-      shareOpen: false,
-    }
-  }
+const VideoItem = ({ user, playlist, playlistVideos, video, videoTitle, videoEtag, videoId, videoChannel, datePublished, duration, togglePlayer, toggleSearchPlayer, togglePlaylistPopup, onAddToPlaylist, onRemoveFromPlaylist, inSearchResults, currentVideoId, autoAdd, orderBy, itsOnLibrary, onAddToLibrary, onRemoveFromLibrary, inRelatedVideos, inLibraryVideos, reorder, fromWatch}) => {
   
-  render() {
-    const durationFormated = Moment.duration(this.props.duration).asMilliseconds() > 3600000
-      ? Moment.utc(Moment.duration(this.props.duration).asMilliseconds()).format("hh:mm:ss")
-      : Moment.utc(Moment.duration(this.props.duration).asMilliseconds()).format("mm:ss")
+  const durationFormated = Moment.duration(duration).asMilliseconds() > 3600000
+    ? Moment.utc(Moment.duration(duration).asMilliseconds()).format("hh:mm:ss")
+    : Moment.utc(Moment.duration(duration).asMilliseconds()).format("mm:ss")
+  
+  const AuthorId = typeof playlist !== 'undefined' ? playlist.AuthorId : null;
+  const extraMeta = duration ? " · Duration: " + durationFormated : null; 
     
-    const AuthorId = typeof this.props.playlist !== 'undefined' ? this.props.playlist.AuthorId : null;
-    const extraMeta = this.props.duration ? " · Duration: " + durationFormated : null; 
-      
-    let deleteButton = null;
+  let deleteButton = null;
 
-    let addButton = <StyledActionButton onClick={() => this.props.togglePlaylistPopup(this.props.video)}>
-      <MaterialIcon icon="playlist_add" color='#fff' />
-    </StyledActionButton>
-    if (this.props.autoAdd) addButton = <StyledActionButton onClick={() => this.props.onAddToPlaylist(this.props.video, this.props.playlist, this.props.autoAdd)}>
-      <MaterialIcon icon="playlist_add" color='#fff' />
-    </StyledActionButton>
+  let addButton = <StyledActionButton onClick={() => togglePlaylistPopup(video)}>
+    <MaterialIcon icon="playlist_add" color='#fff' />
+  </StyledActionButton>
+  if (autoAdd) addButton = <StyledActionButton onClick={() => onAddToPlaylist(video, playlist, autoAdd)}>
+    <MaterialIcon icon="playlist_add" color='#fff' />
+  </StyledActionButton>
 
-    let videoTrigger = null;
+  let videoTrigger = null;
 
-    
-    if (this.props.fromWatch) {
-      videoTrigger = 
-        <StyledVideoInfoLink to={`/watch/${this.props.videoId}`}>
-          <VideoMeta>{this.props.videoChannel}</VideoMeta>
-          <VideoItemTitle>{this.props.videoTitle}</VideoItemTitle>
-          <VideoMeta>Published: {this.props.datePublished}{extraMeta}</VideoMeta>
-        </StyledVideoInfoLink>
-    } else if (this.props.inSearchResults === true) {
-      videoTrigger = 
-      <StyledVideoInfo onClick={() => this.props.toggleSearchPlayer(this.props.video)}>
-          <VideoMeta>{this.props.videoChannel}</VideoMeta>
-          <VideoItemTitle>{this.props.videoTitle}</VideoItemTitle>
-          <VideoMeta>Published: {this.props.datePublished}{extraMeta}</VideoMeta>
-        </StyledVideoInfo>
-    } else {
-      videoTrigger = 
-        <StyledVideoInfo onClick={() => this.props.togglePlayer(this.props.video, this.props.playlist, this.props.playlistVideos)}>
-          <VideoMeta>{this.props.videoChannel}</VideoMeta>
-          <VideoItemTitle>{this.props.videoTitle}</VideoItemTitle>
-          <VideoMeta>Published: {this.props.datePublished}{extraMeta}</VideoMeta>
-        </StyledVideoInfo>
-    }
+  
+  if (fromWatch) {
+    videoTrigger = 
+      <StyledVideoInfoLink to={`/watch/${videoId}`}>
+        <VideoMeta>{videoChannel}</VideoMeta>
+        <VideoItemTitle>{videoTitle}</VideoItemTitle>
+        <VideoMeta>Published: {datePublished}{extraMeta}</VideoMeta>
+      </StyledVideoInfoLink>
+  } else if (inSearchResults === true) {
+    videoTrigger = 
+    <StyledVideoInfo onClick={() => toggleSearchPlayer(video)}>
+        <VideoMeta>{videoChannel}</VideoMeta>
+        <VideoItemTitle>{videoTitle}</VideoItemTitle>
+        <VideoMeta>Published: {datePublished}{extraMeta}</VideoMeta>
+      </StyledVideoInfo>
+  } else {
+    videoTrigger = 
+      <StyledVideoInfo onClick={() => togglePlayer(video, playlist, playlistVideos)}>
+        <VideoMeta>{videoChannel}</VideoMeta>
+        <VideoItemTitle>{videoTitle}</VideoItemTitle>
+        <VideoMeta>Published: {datePublished}{extraMeta}</VideoMeta>
+      </StyledVideoInfo>
+  }
 
-    let libraryButton = null;
+  let libraryButton = null;
 
-    //Add to library button
-    if (this.props.user !== null) {
-      if (this.props.itsOnLibrary === true) {
-        libraryButton = 
-        <StyledLibraryButtonCheck onClick={() => this.props.onRemoveFromLibrary(this.props.video)}>
-          <span>
-            <MaterialIcon icon="check" color='#fff' />
-            <MaterialIcon icon="close" color='#fff' />
-          </span>
-        </StyledLibraryButtonCheck>
-      }
-      else if (this.props.itsOnLibrary === false) {
-        libraryButton = 
-        <StyledLibraryButton onClick={() => this.props.onAddToLibrary(this.props.video, true)}>
-          <MaterialIcon icon="add" color='#fff' />
-        </StyledLibraryButton>
-      }
-
-    } else {
+  //Add to library button
+  if (user !== null) {
+    if (itsOnLibrary === true) {
       libraryButton = 
-      <StyledLibraryButton onClick={() => this.props.togglePlaylistPopup(this.props.video)}>
+      <StyledLibraryButtonCheck onClick={() => onRemoveFromLibrary(video)}>
+        <span>
+          <MaterialIcon icon="check" color='#fff' />
+          <MaterialIcon icon="close" color='#fff' />
+        </span>
+      </StyledLibraryButtonCheck>
+    }
+    else if (itsOnLibrary === false) {
+      libraryButton = 
+      <StyledLibraryButton onClick={() => onAddToLibrary(video, true)}>
         <MaterialIcon icon="add" color='#fff' />
       </StyledLibraryButton>
     }
 
-    if (this.props.user !== null) {
-      if (this.props.inSearchResults === true || this.props.user.uid !== AuthorId || this.props.inRelatedVideos) {
-        deleteButton = null
-        if (this.props.inLibraryVideos && this.props.orderBy === "custom" && this.props.reorder) {
-          libraryButton = <StyledDragHandle><MaterialIcon icon="drag_handle" color='#fff' /></StyledDragHandle>
-        }
-      } else {
-        deleteButton = <StyledActionButton onClick={() => this.props.onRemoveFromPlaylist(this.props.videoId, this.props.playlist)}>
-          <MaterialIcon icon="delete_forever" color='#fff' />
-        </StyledActionButton>
-        
-        if (this.props.orderBy === "custom" && this.props.reorder) {
-          libraryButton = <StyledDragHandle><MaterialIcon icon="drag_handle" color='#fff' /></StyledDragHandle>
-        }
+  } else {
+    libraryButton = 
+    <StyledLibraryButton onClick={() => togglePlaylistPopup(video)}>
+      <MaterialIcon icon="add" color='#fff' />
+    </StyledLibraryButton>
+  }
+
+  if (user !== null) {
+    if (inSearchResults === true || user.uid !== AuthorId || inRelatedVideos) {
+      deleteButton = null
+      if (inLibraryVideos && orderBy === "custom" && reorder) {
+        libraryButton = <StyledDragHandle><MaterialIcon icon="drag_handle" color='#fff' /></StyledDragHandle>
+      }
+    } else {
+      deleteButton = <StyledActionButton onClick={() => onRemoveFromPlaylist(videoId, playlist)}>
+        <MaterialIcon icon="delete_forever" color='#fff' />
+      </StyledActionButton>
+      
+      if (orderBy === "custom" && reorder) {
+        libraryButton = <StyledDragHandle><MaterialIcon icon="drag_handle" color='#fff' /></StyledDragHandle>
       }
     }
-
-
-    return(
-      <StyledVideoItem className={this.props.currentVideoId === this.props.videoId && 'active'}>
-        {libraryButton}
-        <StyledContent>
-          {videoTrigger}
-          <StyledActions>
-            {addButton}
-            {deleteButton}
-          </StyledActions>
-        </StyledContent>
-      </StyledVideoItem>
-    );
   }
-}
+
+
+  return(
+    <StyledVideoItem className={currentVideoId === videoId && 'active'}>
+      {libraryButton}
+      <StyledContent>
+        {videoTrigger}
+        <StyledActions>
+          {addButton}
+          {deleteButton}
+        </StyledActions>
+      </StyledContent>
+    </StyledVideoItem>
+  );
+};
 
 export default VideoItem;
