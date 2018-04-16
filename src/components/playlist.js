001dfbe4303f73cf67ea0b8c4ef0e26af1ec2c13
Kevin Chanquín <kevin@royalestudios.com>
2018-04-16 10:13:57 -0600

    ADD playlist_header

--- src/components/playlist.js
+++ src/components/playlist.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import firebase from 'firebase';
 import '@firebase/firestore';
 import styled from 'styled-components';
 import { css } from 'styled-components';
-import MaterialIcon from 'material-icons-react';
 import _ from 'lodash';
 
 import SharePopup from './share_popup';
 import VideoListContainer from './video_list_container';
 import PlaylistOptionsPopup from './playlist_options_popup';
+import PlaylistHeader from './playlist_header';
 
 const sizes = {
   small: 360,
@@ -40,38 +39,6 @@ const PlaylistContainer = styled.div`
     height: calc(100vh - 100px);
   `}
 `;
-const StyledHeaderContainer = styled.div`
-  display: flex;
-  flex: 1 0 auto;
-  position: relative;
-`;
-const StyledBackButton = styled.a`
-  font-size: 24px;
-  margin-right: 10px;
-  transition: all .5s ease;
-  cursor: pointer;
-  display: block;
-`;
-const StyledHeader = styled.div`
-  display: block;
-  border-bottom: 1px solid rgba(255,255,255,0.1);
-  padding-bottom: 10px;
-  transition: all .5s ease-out;
-  width: 100%;
-  overflow: hidden;
-  ${props => props.scrolling && `
-    height: 60px;
-  `}
-`;
-const StyledPlaylistName = styled.h1`
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  transition: all .5s ease;
-  ${props => props.scrolling && `
-    font-size: 24px;
-  `}
-`;
 const StyledNoFoundContent = styled.div`
   width: 100%;
   height: calc(100vh - 354px);
@@ -86,78 +53,6 @@ const StyledNoFoundContent = styled.div`
   h1{
     margin-bottom: 40px;
   }
-`;
-const StyledHeaderActions = styled.div`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: column;
-  transition: all .5s ease-out;
-  ${media.xmedium`
-    flex-direction: row;
-  `}
-  ${props => props.scrolling && `
-    margin-top: -80px;
-  `}
-`;
-const StyledPlaylistInfo = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  padding-top: 10px;
-  ${media.xmedium`
-    padding-top: 0;
-  `}
-`;
-const StyledLabel = styled.h3`
-  font-size: 10px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-weight: 400;
-  transition: all .3s;
-  ${props => props.scrolling && `
-    opacity: 0;
-    visibility: hidden;
-  `}
-`;
-const StyledAuthorLink = styled(Link)`
-  font-size: 10px;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  font-weight: 400;  
-  margin-bottom: 6px;
-  color: #fff;
-  text-decoration: none;
-`;
-const StyledPlaylistActions = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin-top: 10px;
-  position: relative;
-  ${media.xmedium`
-    margin-top: 0;
-    justify-content: flex-end;
-  `}
-`;
-const StyledButtonGroup = styled.div`
-  display: flex;
-`;
-const StyledPlaylistTags = styled.div`
-  width: 100%;
-  display: flex;
-  align-items: center;
-  margin: 10px 0;
-  overflow-x: auto;
-  overflow-y: hidden;
-  transition: all .5s;
-  ${media.xmedium`
-    padding-top: 0;
-  `}
-  ${props => props.scrolling && `
-    opacity: 0;
-    visibility: hidden;
-  `}
 `;
 const PlaylistActions = styled.a`
   position: relative;
@@ -174,82 +69,8 @@ const PlaylistActions = styled.a`
     border: 1px solid rgba(255,255,255,1);
   }
 `;
-const PlaylistActionsNone = styled.span`
-  position: relative;
-  display: inline-block;
-  letter-spacing: 2px;
-  text-transform: uppercase;
-  font-size: 10px;
-  padding: 10px;
-  padding-left: 0;
-  transition: all .3s ease;
-  overflow: hidden;
-`;
 const StyledPopupContainer = styled.div`
   position: relative;
-`;
-const StyledButton = styled.a`
-  opacity: .6;
-  cursor: pointer;
-  transition: all .3s ease;
-  margin-left: 10px;
-  display: block;
-  &:hover{
-    opacity: 1;
-  }
-`;
-const StyledButtonTagMore = styled.a`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  opacity: .8;
-  cursor: pointer;
-  transition: all .3s ease;
-  padding: 10px;
-  border: 1px solid rgba(255,255,255,0.1);
-  border-radius: 20px;
-  margin-right: 10px;
-  white-space: nowrap;
-  &:hover{
-    opacity: 1;
-    border: 1px solid rgba(255,255,255,1);
-  }
-`;
-const StyledDivTag = styled.div`
-  display: flex;
-  white-space: nowrap;
-  align-items: center;
-  justify-content: center;
-  opacity: .8;
-  cursor: pointer;
-  transition: all .3s ease;
-  background-color: rgba(255, 255, 255, 0.1);
-  border-radius: 20px;
-  margin-right: 10px;
-  height: 35px;
-  padding: 0 6px;
-  &:hover{
-    opacity: 1;
-    background-color: rgba(255, 255, 255, 0.2);
-  }
-`;
-const StyledButtonTagRemove = styled.a`
-  cursor: pointer;
-  transition: all .3s ease;
-  margin-top: 1px;
-  padding: 9px 0 9px 4px;
-  opacity: 0.4;
-  &:hover{
-    opacity: 1;
-  }
-`;
-const StyledButtonTagName = styled(Link)`
-  cursor: pointer;
-  transition: all .3s ease;
-  font-size: 14px;
-  padding: 12px 8px;
-  text-decoration: none;
-  color: #fff;
 `;
 
 class Playlist extends Component {
@@ -371,29 +192,6 @@ class Playlist extends Component {
           playlistVideos: _.orderBy(nextState.playlistVideos, [nextState.orderBy], [nextState.orderDirection])
         })
       }
-    }
-
-    if (this.state.tags !== nextState.tags) {
-      //Map tags
-      const tagItems = nextState.tags ? nextState.tags.map((tag, i) => { //if tags exist
-        const newTags = [...nextState.tags];
-        newTags.splice(i, 1);
-
-        //add remove button if user is logged in and owns the playlist      
-        const tagRemove = nextProps.user !== null && nextProps.user.uid === nextState.playlist.AuthorId ?
-        <StyledButtonTagRemove onClick={() => nextProps.onRemoveTag(newTags, nextState.playlist)}>
-          <MaterialIcon icon="clear" color='#fff' size="18px" />
-        </StyledButtonTagRemove> : null;
-        return (
-          <StyledDivTag key= {i}>
-            {tagRemove}
-            <StyledButtonTagName to={`/tags/${tag}`}>
-              {tag}
-            </StyledButtonTagName>
-          </StyledDivTag>)
-      }) : null;
-
-      this.setState({tagItems});
     }
   }
 
@@ -527,93 +325,30 @@ class Playlist extends Component {
 
     const playlist = this.state.playlist;
     const playlistName = this.state.playlist.playlistName;
-    const playlistAuthor = this.state.playlist.Author;
     const playlistFollowers = this.state.playlistPublicInfo.followers;
     
-    //Set tags
-    let addTags = null;
-    let playlistTags = null;
-
-    //if user is logged in and owns the playlist
-    if (this.props.user !== null && this.props.user.uid === playlist.AuthorId) {
-      //add button to add tags
-      addTags = <StyledButtonTagMore onClick={() => this.props.toggleAddTagPopup(this.state.playlist)}>
-        <MaterialIcon icon="add" color='#fff' size="14px" />Add Tag
-      </StyledButtonTagMore>
-
-      playlistTags = <StyledPlaylistTags scrolling={this.state.scrolling ? 1 : 0}>
-        {this.state.tagItems}
-        {addTags}
-      </StyledPlaylistTags>
-    } 
-    //if user doesn't owns the playlist but it has tags
-    else if (this.state.tagItems) {
-      playlistTags = <StyledPlaylistTags scrolling={this.state.scrolling ? 1 : 0}>
-        {this.state.tagItems}
-      </StyledPlaylistTags>
-    }
-
-    //Set Follow for playlists, related videos and sortable list
-    let followButton = null;
-    let reorderButton = null;
-
-    if (this.props.user !== null ) {
-      if (this.props.user.uid !== playlist.AuthorId) {
-        followButton = 
-        <PlaylistActions onClick={() => this.props.onPlaylistFollow(playlist, playlistFollowers)}>
-          {playlistFollowers} Followers
-        </PlaylistActions>
-
-      } else {
-        followButton = <PlaylistActionsNone> {playlistFollowers} Followers </PlaylistActionsNone>
-        
-        if (this.state.orderBy === 'custom') {
-
-          if (this.state.reorder) {
-            reorderButton = 
-            <StyledButton onClick={this.onToggleReorder}>
-              <MaterialIcon icon="done" color='#fff' />
-            </StyledButton>
-          } 
-          
-          else {
-            reorderButton = 
-            <StyledButton onClick={this.onToggleReorder}>
-              <MaterialIcon icon="format_line_spacing" color='#fff' />
-            </StyledButton>
-          }
-        } 
-      }
-
-    } else {
-      followButton = <PlaylistActionsNone> {playlistFollowers} Followers </PlaylistActionsNone>
-    }
-
     return(
       <PlaylistContainer>
-        <StyledHeaderContainer>
-          <StyledBackButton onClick={() => window.history.back()}><MaterialIcon icon="arrow_back" color='#fff' /></StyledBackButton>
-          <StyledHeader scrolling={this.state.scrolling ? 1 : 0}>
-            <StyledAuthorLink to={`/users/${playlist.AuthorId}`}>{playlistAuthor}'s</StyledAuthorLink>
-            <StyledPlaylistName scrolling={this.state.scrolling ? 1 : 0}> {playlistName} </StyledPlaylistName>
-            {playlistTags}
-            <StyledHeaderActions scrolling={this.state.scrolling ? 1 : 0}>
-              <StyledPlaylistInfo>
-                <StyledLabel scrolling={this.state.scrolling ? 1 : 0}>{playlist.videoCount} Videos in this playlist</StyledLabel>
-              </StyledPlaylistInfo>
-              <StyledPlaylistActions>
-                {followButton}
-                <StyledButtonGroup>
-                  {reorderButton}
-                  <StyledButton onClick={this.toggleShare} >
-                    <MaterialIcon icon="share" color='#fff' />
-                  </StyledButton>
-                  <StyledButton onClick={this.togglePlaylistsOptions}><MaterialIcon icon="more_vert" color='#fff' /></StyledButton>
-                </StyledButtonGroup>
-              </StyledPlaylistActions>
-            </StyledHeaderActions>
-          </StyledHeader>
-        </StyledHeaderContainer>
+        <PlaylistHeader
+          type="playlist"
+          owner={this.props.user !== null && this.props.user.uid === playlist.AuthorId}
+          follow={true}
+          reorder={this.state.orderBy === "custom" ? this.state.reorder : null}
+          back={true}
+          scrolling={this.state.scrolling}
+
+          playlist={playlist}
+          playlistName={playlist.playlistName}
+          playlistFollowers={playlistFollowers}
+          onPlaylistFollow={this.props.onPlaylistFollow}
+          togglePlaylistsOptions={this.togglePlaylistsOptions}
+          toggleShare={this.toggleShare}
+          onToggleReorder={this.onToggleReorder}
+          
+          tags={this.state.tags}
+          onRemoveTag={this.props.onRemoveTag}
+          toggleAddTagPopup={this.props.toggleAddTagPopup}
+        />
         <StyledPopupContainer>
           <SharePopup
             open={this.state.shareOpen}
